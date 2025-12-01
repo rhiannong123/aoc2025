9565ff7408a95205b0bcf343ee7d3022e6919ade
Rhiannon Griffin Sanchez <rhiannon.d-ctr.griffin.sanchez@faa.gov>
2025-12-01 17:03:16 -0600

    Day 1, part 2 works.

--- day1.py
+++ day1.py
@@ -8,8 +8,7 @@ num = 1
 #lines = aoc.input_readlines(num, test=True)
 lines = aoc.input_readlines(num)
 
-position = 50
-print(lines)
+STARTING_POSITION = 50
 
 def turn_dial(initial, line):
     '''
@@ -19,37 +18,70 @@ def turn_dial(initial, line):
     l_or_r = line[0]
     increment = int(line[1:])
     
+    if initial == 0:
+        start_at_zero = True
+        first_time_in_while_flag = 1
+    else:
+        start_at_zero = False
+        first_time_in_while_flag = 1
+
+    count_num_of_zero_crossings = 0
+        
     if l_or_r == 'L':
         end_actual = initial - increment 
         while end_actual < 0:
             end_actual += 100
+            count_num_of_zero_crossings += 1
+            if (start_at_zero == True) & (first_time_in_while_flag == 1):
+                first_time_in_while_flag = 0
+                count_num_of_zero_crossings -= 1
     else:
         end_actual = initial + increment
         while end_actual > 99:
             end_actual -= 100
+            count_num_of_zero_crossings += 1
             
     if end_actual == 0:
+        if l_or_r == 'L':
+            count_num_of_zero_crossings += 1
         end_at_zero = True
     else:
         end_at_zero = False
 
+    print(f"Initial position of {initial} with command {line} results in end position of {end_actual} and number of zero crossings of {count_num_of_zero_crossings}")
+    
+    return end_actual, end_at_zero, count_num_of_zero_crossings
             
-    print(initial, line, end_actual, end_at_zero)
-    return end_actual, end_at_zero
-            
-def main(lines, position):
+def main(lines):
     
     ## Part 1
+    position = STARTING_POSITION
     end_at_zero_sum = 0
     for line in lines:
-        position, end_at_zero = turn_dial(position, line)
+        position, end_at_zero, _ = turn_dial(position, line)
         if end_at_zero:
             end_at_zero_sum += 1
             
     part1_answer = end_at_zero_sum
     print(f'The answer to Part 1: {part1_answer}')
         
+    ## Part 2
+    position = STARTING_POSITION
+    nonzero_zero_crossings = []
+    for line in lines:
+        position, _, counts = turn_dial(position, line)
+        if counts > 0:
+            nonzero_zero_crossings.append(counts)
+            
+    part2_answer = sum(nonzero_zero_crossings)
+    print(f'The answer to Part 2: {part2_answer}')         
+    
+    ## TEST AREA
+    '''
+    position, _, counts = turn_dial(50, "R1000") 
+    print(position, counts)  
+    '''
         
 if __name__ == "__main__":
-    main(lines, position)
+    main(lines)
     
