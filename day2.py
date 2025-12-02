3512aa9b9a8c2964aaf413ae219e9625956c2e97
Rhiannon Griffin Sanchez <rhiannon.d-ctr.griffin.sanchez@faa.gov>
2025-12-02 13:23:54 -0600

    Day 2, part 1 works.

--- day2.py
+++ day2.py
@@ -3,34 +3,59 @@ import pandas as pd
 
 
 num = 2
+
 lines = aoc.input_readlines(num, test=True)
-#lines = aoc.input_readlines(num)
+lines = aoc.input_readlines(num)
 
-STARTING_POSITION = 50
+DEBUG = True
+DEBUG = False
 
+def process_line(line, debug):
+    start, end = line.split('-')
+    
+    ## PART 1
+    
+    # Iterate over range, keep track of sum of invalid IDs in count_invalid
+    count_invalid = 0
+    for i in range(int(start), int(end)+1):
+        istr = str(i)
+        if len(istr) % 2 != 0:
+            # length of istr is odd - can't be repeat sequence - don't care
+            continue
+        half_length = int(len(istr) / 2)
+        first_half = istr[:half_length]
+        second_half = istr[half_length:]
+        if debug:
+            print(f'ID of {istr}, has a first half of {first_half}, second half of {second_half}')
+        # Check if first half of ID matches second, if yes, it's invalid, keep track of sum
+        if first_half == second_half:
+            if debug:
+                print(f'####### Found an invalid ID: {first_half}')
+            count_invalid += int(istr)
             
-def main(lines):
+    return count_invalid
+            
+    
+
+def main(lines,debug=DEBUG):
     
     ## Part 1
-    position = STARTING_POSITION
-    end_at_zero_sum = 0
+    lines = lines[0].split(',')
+    count_invalids = []
     for line in lines:
-        position, end_at_zero, _ = turn_dial(position, line)
-        if end_at_zero:
-            end_at_zero_sum += 1
+        #if line != '11-22':
+        #    continue
+        count_invalid = process_line(line,debug)
+        if count_invalid != 0:
+            count_invalids.append(count_invalid)
+        
             
-    part1_answer = end_at_zero_sum
+    part1_answer = sum(count_invalids)
     print(f'The answer to Part 1: {part1_answer}')
         
     ## Part 2
-    position = STARTING_POSITION
-    nonzero_zero_crossings = []
-    for line in lines:
-        position, _, counts = turn_dial(position, line)
-        if counts > 0:
-            nonzero_zero_crossings.append(counts)
-            
-    part2_answer = sum(nonzero_zero_crossings)
+
+    part2_answer = 'TBD'
     print(f'The answer to Part 2: {part2_answer}')         
     
     ## TEST AREA
@@ -40,5 +65,5 @@ def main(lines):
     '''
         
 if __name__ == "__main__":
-    main(lines)
+    main(lines,debug=DEBUG)
     
